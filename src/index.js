9948d73b59fa11a028362b777539ca697930af54
Awesome Henry <henryebomah@gmail.com>
2021-01-12 08:59:53 -0800

    Initial

---
+++ src/index.js
@@ -0,0 +1,12 @@
+//basic starting code 
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import RouletteGun from './state-drills/RouletteGun'
+
+ReactDOM.render(
+  
+        <RouletteGun />
+        ,
+  document.getElementById('root')
+)
